15bb015f9f353231a47a5f828ffadf2f1aca3409
taise <taise515@gmail.com>
2016-06-08 20:30:29 +0900

    Fix: usersでnilで表示されない対応

--- models/pg_user.rb
+++ models/pg_user.rb
@@ -3,19 +3,29 @@ class PgUser < Redshift
 
   def self.find_with_summary
     sql = <<'EOS'
+WITH queries AS (
+  SELECT
+    userid,
+    COUNT(*) AS count_query,
+    MIN(starttime) AS started_at,
+    MAX(starttime) AS last_at
+  FROM
+    stl_query
+  GROUP BY
+    userid
+)
+
 SELECT
-  userid::INTEGER,
-  TRIM(usename) AS usename,
-  COUNT(*) AS count_query,
-  MIN(starttime) AS started_at,
-  MAX(starttime) AS last_at
+  usesysid::INTEGER AS id,
+  TRIM(usename) AS name,
+  count_query,
+  started_at,
+  last_at
 FROM
   pg_user
-  LEFT JOIN stl_query ON
-    pg_user.usesysid = stl_query.userid
-GROUP BY
-  userid,
-  usename
+  LEFT JOIN queries ON
+    pg_user.usesysid = queries.userid
+ORDER BY id
 EOS
     find_by_sql(sql)
   end
