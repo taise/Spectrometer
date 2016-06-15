5fb5d30d2d8681a7b422784318d1d5f9adac5f8c
taise <taise515@gmail.com>
2016-06-15 20:41:52 +0900

    Rubocop: auto correct models

--- models/redshift.rb
+++ models/redshift.rb
@@ -4,5 +4,5 @@ require 'active_record'
 class Redshift < ActiveRecord::Base
   self.abstract_class = true
   config = YAML.load_file('./config/database.yml')
-  self.establish_connection config['development']
+  establish_connection config['development']
 end
